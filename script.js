805dea6c43ccd20b855b6e4fc842b4ef0ed5d1a8
Julio Martin <faria@DESKTOP-TLUUL68>
2024-01-23 20:43:35 -0300

    Objetos

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 /* document.getElementById("demostracion").innerHTML =
   "Este texto esta en un archivo externog JS"; */
 /* console.log("Este comentario va a salir en consola"); */
-let numero= 25;
+/* let numero= 25; */
+
 /* FUNCIÓN COMÚN/CLÁSICA */
-function suma(numero){
+/* function suma(numero){
   return 25 + 25;
 }
 
-console.log(`El valor a sumar es ${numero} el resultado de la suma es `, suma(numero));
+console.log(`El valor a sumar es ${numero} el resultado de la suma es `, suma(numero)); */
 
 /* FUNCIÓN FLECHA/ ARROW FUNCTION */
 
-let sumaFlecha = (numero) => numero + 75;
+/* let sumaFlecha = (numero) => numero + 75;
 
 console.log (`La función flecha sumando un valor como ${numero} de `,sumaFlecha(numero));
+ */
+
+/* let auto1 = {
+  marca : 'FIAT',
+  modelo: 'UNO',
+  peso: 1500,
+  color: 'Rojo'
+}
+
+
+let auto2 = {
+  marca : 'RENAULT',
+  modelo: 'DOCE',
+  peso: 2500,
+  color: 'Azul'
+}
+
+console.log(`Auto 1:`, auto1);
+console.log(`Auto 2:` , auto2); */
